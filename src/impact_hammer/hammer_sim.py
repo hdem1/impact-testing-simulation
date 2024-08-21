2a00daa24c9b5fd7d69ce9733c36bfd9e50b9168
Henry Demarest <46830987+hdem1@users.noreply.github.com>
2024-08-21 01:46:55 -0400

    finishing adding different types of simulation

--- src/impact_hammer/hammer_sim.py
+++ src/impact_hammer/hammer_sim.py
@@ -28,7 +28,7 @@ class HammerSimulation:
     initial_conditions = [np.deg2rad(175), 0]
     ODE_func = None
     dt = 0.001 # s
-    duration = 2 # s
+    duration = 100 # s
     sol = None
 
     def __init__(self, structure_json_filename):
@@ -42,7 +42,10 @@ class HammerSimulation:
         if self.ODE_func == None:
             print("You must configure the simulation before running it")
             return
-        self.sol = solve_ivp(self.ODE_func, [0, self.duration], (self.initial_conditions[0], self.initial_conditions[1]), t_eval = np.linspace(0, self.duration, round(self.duration/self.dt)))
+        def event(t,y):
+            return y[0];
+        event.terminal = True
+        self.sol = solve_ivp(self.ODE_func, [0, self.duration], (self.initial_conditions[0], self.initial_conditions[1]), t_eval = np.linspace(0, self.duration, round(self.duration/self.dt)), events=event)
 
     def get_t(self):
         if self.sol == None:
@@ -79,3 +82,40 @@ class HammerSimulation:
             print("You must run the simulation before attempting to access its parameters")
             return
         plot_mult(self.get_t(), [self.get_theta_deg(), self.get_speeds()], "Time [s]", [r'$\theta$', r'Speed [m/s]'], title)
+
+
+class SimpleHammerSimulation(HammerSimulation):
+    def __init__(self,structure_json_filename):
+        super().__init__(structure_json_filename)
+
+        def hammer_ODE(t, y):
+            ang_deriv = y[1]
+            ang_vel_deriv = - G_ACCEL * np.sin(y[0]) / self.hammer_structure.L_hammer
+            return ang_deriv, ang_vel_deriv
+        
+        self.configure_ODE_func(hammer_ODE)
+
+class TorsionalSpringSimulation(HammerSimulation):
+    def __init__(self,structure_json_filename, max_torque, rad_range):
+        super().__init__(structure_json_filename)
+
+        def hammer_ODE(t, y):
+            k_s = max_torque / rad_range #N/rad
+            ang_equil = np.pi - rad_range
+            ang_deriv = y[1]
+            ang_vel_deriv = (-1 * k_s * (y[0] - ang_equil) - G_ACCEL * np.sin(y[0]))/ self.hammer_structure.L_hammer
+            return ang_deriv, ang_vel_deriv
+
+        self.configure_ODE_func(hammer_ODE)
+
+class MotorizedHammerSimulation(HammerSimulation):
+    def __init__(self,structure_json_filename, torque_func):
+        super().__init__(structure_json_filename)
+        
+        def hammer_ODE(t, y):
+            motor_torque = torque_func(y[1])
+            ang_deriv = y[1]
+            ang_vel_deriv = (-1 * motor_torque - G_ACCEL * np.sin(y[0]))/ self.hammer_structure.L_hammer
+            return ang_deriv, ang_vel_deriv
+
+        self.configure_ODE_func(hammer_ODE)
